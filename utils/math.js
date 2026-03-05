cecdcbd2daa8bca5b45f05792810272e388b02fe
harisvijay <53299103+harisvijay@users.noreply.github.com>
2026-03-05 15:35:14 +0530

    Create math.js

---
+++ utils/math.js
@@ -0,0 +1,28 @@
+function addNumbers(a,b){
+
+    return a + b
+
+}
+
+function divide(a,b){
+
+    return a/b
+
+}
+
+function slowLoop(){
+
+    let total = 0
+
+    for(let i=0;i<100000000;i++){
+        total = total + i
+    }
+
+    return total
+}
+
+module.exports = {
+    addNumbers,
+    divide,
+    slowLoop
+}
